dc2cf2b3e80e8fb59903817d37f9ece941100d5d
matthew.bandel <mcmbandel@gmail.com>
2022-07-16 21:09:10 -0700

    adding racquetball theory

---
+++ racquetballs.py
@@ -0,0 +1,140 @@
+# racquetballs.py to test particle in a box hypotheses
+
+# import system tools
+import os, sys
+import json
+
+# import time
+from time import time, sleep
+
+# import trig functions
+from math import sin, cos, pi, exp, log
+
+# import numpy
+import numpy
+from numpy.random import random, rand, randint
+from numpy import sin, cos, pi, exp, log, sqrt
+
+# import matplotlib for plots
+from matplotlib import pyplot
+from matplotlib import gridspec
+from matplotlib import colorbar
+from matplotlib import colors as Colors
+from matplotlib import style as Style
+Style.use('fast')
+
+
+# play racquetball
+def racquet(steps=100, frequency=1.0, left=0.0, right=20.0, teleportation=10.0):
+    """Simulate a session of quantum racquetball.
+
+    Arguments:
+        steps: int, number of steps
+        frequency: float, maximum random step distance
+        left: float, left wll position
+        right: float, right wall position
+        teleportation: float, teleport distance
+
+    Returns:
+        None
+    """
+
+    # begin segments
+    segments = []
+
+    # set beginning at far left, with rightward movement
+    first = left
+    rightward = True
+
+    # create random lengths
+    lengths = numpy.random.rand(steps,) * frequency
+
+    # create directions at random
+    directions = numpy.random.choice(a=[False, True], size=(steps, ))
+
+    # for each step
+    for length, direction in zip(lengths, directions):
+
+        # first non teleport, then teleport
+        for teleport in (False, True):
+
+            # change length
+            if teleport:
+
+                # change length
+                length = teleportation
+                rightward = direction
+
+            # if rightward
+            if rightward:
+
+                # add
+                second = first + length
+
+                # check for crossing boundary
+                if second > right:
+
+                    # add first segment
+                    segment = (first, right, teleport)
+                    segments.append(segment)
+
+                    # create other portion
+                    portion = length - (right - first)
+                    segment = (right, right - portion, teleport)
+                    segments.append(segment)
+                    rightward = False
+
+                # otherwise
+                else:
+
+                    # just add the segment
+                    segment = (first, second, teleport)
+                    segments.append(segment)
+
+            # otherwise
+            else:
+
+                # substart
+                second = first - length
+
+                # check for crossing boundary
+                if second < left:
+
+                    # add first segment
+                    segment = (first, left, teleport)
+                    segments.append(segment)
+
+                    # create other portion
+                    portion = length - (first - left)
+                    segment = (left, left + portion, teleport)
+                    segments.append(segment)
+                    rightward = True
+
+                # otherwise
+                else:
+
+                    # just add the segment
+                    segment = (first, second, teleport)
+                    segments.append(segment)
+
+            # reassign first
+            first = segments[-1][1]
+
+    # use matplotlib
+    pyplot.clf()
+    for index, segment in enumerate(segments):
+
+        # set color
+        color = 'b-'
+        if segment[2]:
+
+            # set to red for teleport
+            color = 'r--'
+
+        # plot segment
+        pyplot.plot([segment[0], segment[1]], [index, index], color)
+
+    # saveplot
+    pyplot.savefig('racquetball.png')
+
+    return None
